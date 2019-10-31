7a2bd76496537be02408e38ea080186b43882dce
Devanshu Brahmbhatt <devanshu.vguj@gmail.com>
2019-11-01 01:23:03 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+const filesToCache = [
+    '/',
+    'css/style.css',
+    'images/mine.jpg',
+    'index.html',
+
+  ];
+  
+  const staticCacheName = 'pages-cache-v1';
+  
+  self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+  
+        // TODO 4 - Add fetched files to the cache
+  
+      }).catch(error => {
+  
+        // TODO 6 - Respond with custom offline page
+  
+      })
+    );
+  });
+
+  self.addEventListener('activate', event => {
+    console.log('Activating new service worker...');
+  
+    const cacheWhitelist = [staticCacheName];
+  
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
