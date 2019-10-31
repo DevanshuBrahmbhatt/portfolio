401e14d767a056cc12b7d6faa4c7f05d6a57ecdb
Devanshu Brahmbhatt <devanshu.vguj@gmail.com>
2019-11-01 05:10:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const filesToCache = [
     '/',
     'css/style.css',
     'images/mine.jpg',
-    'index.html',
+    'index.html'
 
   ];
   
@@ -66,3 +66,33 @@ self.addEventListener('fetch', event => {
       })
     );
   });
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/css/style.css`,
+        `/images/mine.jpg`
+       
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
