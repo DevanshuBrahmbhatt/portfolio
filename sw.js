dc6d64e0d8ba5446e34a05b16083e6b574d94438
Devanshu Brahmbhatt <devanshu.vguj@gmail.com>
2019-11-01 05:12:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -67,32 +67,3 @@ self.addEventListener('fetch', event => {
     );
   });
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `/`,
-        `/index.html`,
-        `/css/style.css`,
-        `/images/mine.jpg`
-       
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
